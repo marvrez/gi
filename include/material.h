854adbb15172af73694fa6cb8e80228b7c498c20
Marvin Reza <marvinreza@outlook.com>
2020-01-14 17:33:07 +0100

    brdfs

--- include/material.h
+++ include/material.h
@@ -1,8 +1,111 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
-class Material {
+#include "vec3.h"
+#include "texture.h"
 
+#include <memory>
+
+// we assume that the input vectors are given in a local coordinate system w.r.t the hit normal
+// this means that cos(theta) = w_in.z, simplifying many of the calculations.
+
+struct HitRecord;
+
+class Material {
+public:
+    virtual Vec3 Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const { return Vec3(0); }
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const { return Vec3(0); }
+    virtual double Pdf(const Vec3& wo, const Vec3& wi) const { return 0; }
+    virtual Vec3 Emitted(const HitRecord& hr) const { return Vec3(0); }
+    virtual bool Emittable() const { return false; }
+};
+
+class Lambertian : public Material {
+private:
+    std::shared_ptr<Texture> albedo;
+public:
+    Lambertian(Texture* t) : albedo(t) {}
+    Lambertian(Vec3 col) : albedo(std::make_shared<SolidTexture>(col)) {}
+
+    virtual Vec3 Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const;
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const;
+    virtual double Pdf(const Vec3& wo, const Vec3& wi) const;
+};
+
+class Specular : public Material {
+private:
+    std::shared_ptr<Texture> albedo;
+    double eta;
+public:
+    Specular(Texture* t, double eta=0) : albedo(t), eta(eta) {}
+    Specular(Vec3 col, double eta=0) : albedo(std::make_shared<SolidTexture>(col)), eta(eta) {}
+
+    virtual Vec3 Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const;
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const;
+};
+
+class DiffuseLight : public Material {
+private:
+    std::shared_ptr<Texture> emit;
+public:
+    DiffuseLight() : emit(std::make_shared<SolidTexture>(Vec3(1.f))) {}
+    DiffuseLight(Texture* t) : emit(t) {}
+    DiffuseLight(Vec3 col) : emit(std::make_shared<SolidTexture>(col)) {}
+
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const;
+    virtual Vec3 Emitted(const HitRecord& hr) const;
+    virtual bool Emittable() const { return true; }
+};
+
+class Isotropic : public Material {
+private:
+    std::shared_ptr<Texture> albedo;
+public:
+    Isotropic(Texture* t) : albedo(t) {}
+    Isotropic(Vec3 col) : albedo(std::make_shared<SolidTexture>(col)) {}
+
+    virtual Vec3 Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const;
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const;
+};
+
+class OrenNayar : public Material {
+private:
+    std::shared_ptr<Texture> albedo;
+    double A, B;
+public:
+    // sigma in degrees
+    OrenNayar(Texture* t, double sigma=20.0);
+    OrenNayar(Vec3 col, double sigma=20.0) : OrenNayar(new SolidTexture(col), sigma) {}
+
+    virtual Vec3 Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const;
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const;
+    virtual double Pdf(const Vec3& wo, const Vec3& wi) const;
+};
+
+class Dielectric : public Material {
+private:
+    std::shared_ptr<Texture> albedo;
+    double eta;
+public:
+    Dielectric(Vec3 col, double eta=1.5) : albedo(new SolidTexture(col)), eta(eta) {}
+    Dielectric(double eta=1.5) : Dielectric(Vec3(1.0), eta) {}
+    Dielectric(Texture* t, double eta=1.5) : albedo(t), eta(eta) {}
+
+    virtual Vec3 Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const;
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const;
+};
+
+class Velvet : public Material {
+private:
+    std::shared_ptr<Texture> albedo;
+    double f;
+public:
+    Velvet(Texture* t, double factor) : albedo(t), f(factor) {}
+    Velvet(Vec3 col, double factor) : albedo(std::make_shared<SolidTexture>(col)), f(factor) {}
+
+    virtual Vec3 Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const;
+    virtual Vec3 Sample(const Vec3& wo, bool* is_specular) const;
+    virtual double Pdf(const Vec3& wo, const Vec3& wi) const;
 };
 
 #endif
---
+++ src/material.cpp
@@ -0,0 +1,175 @@
+#include "material.h"
+
+#include "utils.h"
+#include "hit.h"
+
+#include <math.h>
+
+
+///////////////////// STATIC UTILITY FUNCTIONS START ////////////////////////////
+
+static inline double Schlick(double cosine, double ref_idx)
+{
+    double r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    r0 = r0*r0;
+    return r0 + (1.0 - r0)*pow(1.0 - cosine, 5);
+}
+
+// these functions must/should be used for vectors in a local coordinate space w.r.t to a given normal
+// this reduces the calculation of dot products down to accessing the z-variable of a vector. 
+
+static inline double AbsCosTheta(const Vec3& w) { return fabs(w.z); }
+static inline double SinTheta(const Vec3& w) { return sqrt(Max(0.0, 1.0 - w.z*w.z)); }
+
+static inline double SinPhi(const Vec3& w)
+{
+    double sintheta = SinTheta(w);
+    return sintheta == 0 ? 0.0 : Clamp(w.y / sintheta, -1.0, 1.0);
+}
+
+static inline double CosPhi(const Vec3& w)
+{
+    double sintheta = SinTheta(w);
+    return sintheta == 0 ? 1.0 : Clamp(w.x / sintheta, -1.0, 1.0);
+}
+
+static inline bool SameHemisphere(const Vec3& wo, const Vec3& wi)
+{
+    return wo.z*wi.z > 0;
+}
+
+static inline bool Refract(const Vec3& v, const Vec3& n, double ratio, Vec3* refracted)
+{
+    Vec3 uv = Normalized(v);
+    double dt = Dot(uv, n);
+    double discriminant = 1.0 - ratio*ratio*(1.0 - dt*dt);
+    if(discriminant <= 0) return false;
+    *refracted = ratio*(uv - n*dt) - n*sqrt(discriminant);
+    return true;
+}
+
+///////////////////// STATIC UTILITY FUNCTIONS END ////////////////////////////
+
+Vec3 Lambertian::Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const
+{
+    return this->albedo->Sample(hr.u, hr.v, hr.position) / M_PI;
+}
+
+Vec3 Lambertian::Sample(const Vec3& wo, bool* is_specular) const
+{
+    Vec3 wi = CosineSampleHemisphere();
+    if(wo.z < 0) wi.z *= -1;
+    *is_specular = false;
+    return wi;
+}
+
+double Lambertian::Pdf(const Vec3& wo, const Vec3& wi) const
+{
+    return SameHemisphere(wo, wi) ? fabs(wi.z) / M_PI : 0.0;
+}
+
+Vec3 Specular::Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const
+{
+    return this->albedo->Sample(hr.u, hr.v, hr.position)*Schlick(fabs(wo.z), this->eta);
+}
+
+Vec3 Specular::Sample(const Vec3& wo, bool* is_specular) const
+{
+    Vec3 wi = { -wo.x, -wo.y, wo.z };
+    *is_specular = true;
+    return wi;
+}
+
+Vec3 DiffuseLight::Sample(const Vec3& wo, bool* is_specular) const
+{
+    *is_specular = false;
+    return Vec3(0);
+}
+
+Vec3 DiffuseLight::Emitted(const HitRecord& hr) const
+{ 
+    return this->emit->Sample(hr.u, hr.v, hr.position);
+}
+
+Vec3 Isotropic::Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const
+{
+    return this->albedo->Sample(hr.u, hr.v, hr.position);
+}
+
+Vec3 Isotropic::Sample(const Vec3& wo, bool* is_specular) const
+{
+    Vec3 wi = Normalized(RandomInUnitSphere());
+    *is_specular = true;
+    return wi;
+}
+
+OrenNayar::OrenNayar(Texture* t, double sigma) : albedo(t)
+{
+    double sigma_rad = DEG2RAD(sigma);
+    double sigma_rad2 = sigma_rad*sigma_rad;
+    this->A = 1.0 - (sigma_rad2 / (2.0*(sigma_rad2 + 0.33)));
+    this->B = 0.45*sigma_rad2 / (sigma_rad2 + 0.09);
+}
+
+Vec3 OrenNayar::Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const
+{
+    double maxcos = 0.0;
+    double sinthetai = SinTheta(wi), sinthetao = SinTheta(wo);
+    if(sinthetai > M_EPS && sinthetao > M_EPS) {
+        double sinphii = SinPhi(wi), cosphii = CosPhi(wi);
+        double sinphio = SinPhi(wo), cosphio = CosPhi(wo);
+        maxcos = Max(0.0, cosphii*cosphio + sinphii*sinphio);
+    }
+    double sinalpha = AbsCosTheta(wi) > AbsCosTheta(wo) ? sinthetao : sinthetai;
+    double tanbeta  = AbsCosTheta(wi) > AbsCosTheta(wo) ? sinthetai / AbsCosTheta(wi) : sinthetao / AbsCosTheta(wo);
+    return this->albedo->Sample(hr.u, hr.v, hr.position)*(this->A + this->B*maxcos*sinalpha*tanbeta) / M_PI;
+}
+
+Vec3 OrenNayar::Sample(const Vec3& wo, bool* is_specular) const
+{
+    Vec3 wi = CosineSampleHemisphere();
+    if(wo.z < 0) wi.z *= -1;
+    *is_specular = false;
+    return wi;
+}
+
+double OrenNayar::Pdf(const Vec3& wo, const Vec3& wi) const
+{
+    return SameHemisphere(wo, wi) ? fabs(wi.z) / M_PI : 0.0;
+}
+
+Vec3 Dielectric::Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const
+{
+    return this->albedo->Sample(hr.u, hr.v, hr.position);
+}
+
+Vec3 Dielectric::Sample(const Vec3& wo, bool* is_specular) const
+{
+    Vec3 normal = wo.z < 0 ? Vec3(0.0, 0.0, -1.0) : Vec3(0.0, 0.0, 1.0);
+    double ratio = wo.z < 0 ? this->eta : 1.0 / this->eta;
+    Vec3 refracted;
+    double reflect_prob = Refract(-wo, normal, ratio, &refracted) ? Schlick(AbsCosTheta(wo), this->eta) : 1.0;
+    *is_specular = true;
+    return RandomUniform() < reflect_prob ? Vec3(-wo.x, -wo.y, wo.z) : refracted;
+}
+
+Vec3 Velvet::Eval(const Vec3& wo, const Vec3& wi, const HitRecord& hr) const
+{
+    double costhetao = Max(0.0, wo.z), costhetai = Max(0.0, wi.z);
+    double sinthetao = sqrt(1.0 - costhetao*costhetao);
+    double horizon_scatter = pow(sinthetao, this->f);
+    return horizon_scatter*costhetai*this->albedo->Sample(hr.u, hr.v, hr.position) / M_PI;
+}
+
+Vec3 Velvet::Sample(const Vec3& wo, bool* is_specular) const
+{
+    Vec3 wi = CosineSampleHemisphere();
+    if(wo.z < 0) wi.z *= -1;
+    *is_specular = false;
+    return wi;
+}
+
+double Velvet::Pdf(const Vec3& wo, const Vec3& wi) const
+{
+    return SameHemisphere(wo, wi) ? fabs(wi.z) / M_PI : 0.0;
+}
