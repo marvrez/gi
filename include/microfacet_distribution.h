37d05108393a95e524b57171cd5599cb1e230d4d
Marvin Reza <marvinreza@outlook.com>
2020-01-15 07:42:57 +0100

    power cosine distribution

---
+++ include/microfacet_distribution.h
@@ -0,0 +1,25 @@
+#ifndef MICROFACET_H
+#define MICROFACET_H
+
+struct Vec3;
+
+class MicrofacetDistribution {
+public:
+    virtual double Eval(const Vec3& wh) const = 0;
+    virtual double Pdf(const Vec3& wo, const Vec3& wi) const = 0;
+    virtual Vec3 Sample(const Vec3& p, const Vec3& wo) const = 0;
+};
+
+class PowerCosineDistribution : public MicrofacetDistribution {
+private:
+    double n; // glossiness with range [0, inf), where 0 is a diffuse surface
+    double norm1; // normalization constant for calculating the pdf for sampling
+    double norm2; // normalization constant for calculating the distribution
+public:
+    PowerCosineDistribution(double exponent);
+    virtual double Eval(const Vec3& wh) const;
+    virtual double Pdf(const Vec3& wo, const Vec3& wi) const;
+    virtual Vec3 Sample(const Vec3& p, const Vec3& wo) const;
+};
+
+#endif
---
+++ src/microfacet_distribution.cpp
@@ -0,0 +1,35 @@
+#include "microfacet_distribution.h"
+
+#include "utils.h"
+#include "vec3.h"
+
+#include <math.h>
+
+PowerCosineDistribution::PowerCosineDistribution(double exponent)
+    : n(exponent)
+{
+    this->norm1 = (exponent+1.0) / (2.0*M_PI);
+    this->norm2 = (exponent+2.0) / M_PI;
+}
+
+double PowerCosineDistribution::Eval(const Vec3& wh) const
+{
+    return this->norm2*pow(fabs(wh.z), this->n);
+}
+
+double PowerCosineDistribution::Pdf(const Vec3& wo, const Vec3& wi) const
+{
+    Vec3 wh = Normalized(wo + wi);
+    if(Dot(wo, wh) <= 0) return 0;
+    return (this->norm1*pow(fabs(wh.z), this->n)) / (4.0*Dot(wo, wh));
+}
+
+Vec3 PowerCosineDistribution::Sample(const Vec3& p, const Vec3& wo) const
+{
+    double costheta = pow(RandomUniform(), 1.0 / (this->n + 1.0));
+    double sintheta = sqrt(Max(0.0, 1.0 - costheta*costheta));
+    double phi = 2.0*M_PI*RandomUniform();
+    Vec3 wh = { sintheta*cos(phi), sintheta*sin(phi), costheta }; // spherical direction
+    if(wh.z*wo.z < 0) wh = -wh; // in case they are not in the same hemisphere
+    return Normalized(Reflect(wo, wh));
+}
