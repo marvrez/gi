236234799e97a3e972790a567d2050147e160132
Marvin Reza <marvinreza@outlook.com>
2020-01-26 00:25:10 +0100

    global include header

---
+++ include/gi.h
@@ -0,0 +1,27 @@
+#ifndef GI_H
+#define GI_H
+
+#include "bbox.h"
+#include "material.h"
+#include "sphere.h"
+#include "camera.h"
+#include "mesh.h"
+#include "microfacet_distribution.h"
+#include "hit.h"
+#include "onb.h"
+#include "surface.h"
+#include "image.h"
+#include "plane.h"
+#include "texture.h"
+#include "import.h"
+#include "ray.h"
+#include "triangle.h"
+#include "kdtree.h"
+#include "renderer.h"
+#include "sampler.h"
+#include "vec3.h"
+#include "mat4.h"
+#include "scene.h"
+#include "utils.h"
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,35 @@
 #include <cstdio>
 
-#include "utils.h"
-#include "sphere.h"
-#include "scene.h"
-#include "camera.h"
-#include "renderer.h"
-#include "microfacet_distribution.h"
+#include "gi.h"
 
-constexpr int w = 500, h = 500, num_samples = 100;
+constexpr int w = 500, h = 500, num_samples = 16;
 constexpr double aspect = (double)w / (double)h;
-
-inline Vec3 HexColor(const int hex) {
-    double r = double((hex >> 16) & 0xff) / 255;
-    double g = double((hex >> 8) & 0xff) / 255;
-    double b = double((hex >> 0) & 0xff) / 255;
-    return Pow(Vec3(r, g, b), 2.2);
-}
 
 int main(int argc, char** argv)
 {
-    Camera cam({0,5,3}, {0,0,0}, {0,0,1}, 45.0, aspect, 0.01);
+    Camera cam({0,-4,3}, {0,0,0}, {0,0,1}, 45.0, aspect, 0.01);
 
+    Material* floor_material = new Lambertian(new CheckeredTexture());
     //Material* floor_material = new OrenNayar(Vec3(0.9), 20);
-    //Material* floor_material = new Lambertian(new CheckeredTexture());
-    Material* floor_material = new OrenNayar(Vec3(0.9), 20);
 
     PowerCosineDistribution* pcd = new PowerCosineDistribution(100);
+    //Material* sphere_material = new Lambertian(HexColor(0x808080));
     //Material* sphere_material = new Dielectric();//{0.4, 0.6, 0.8});
-    //Material* sphere_material = new Microfacet(HexColor(0xFFF0A5), pcd, 2.0);
-    Material* sphere_material = new FresnelBlend(HexColor(0xFFF0A5), HexColor(0xFFF0A5), pcd);
+    Material* sphere_material = new Microfacet(HexColor(0xFFF0A5), pcd, 2.0);
+    //Material* sphere_material = new FresnelBlend(HexColor(0xFFF0A5), HexColor(0xFFF0A5), pcd);
     //Material* sphere_material = new Specular(HexColor(0x808080));
 
     Scene scene;
-    scene.Add(new Sphere({0,0,1}, 1,  sphere_material));
+    //scene.Add(new Sphere({0,0,1}, 1,  sphere_material));
+    auto mesh = ReadSTL("dragon.stl", sphere_material);
+    //auto mesh = ReadSTL("minified.stl", sphere_material);
+    //mesh->Rotate({0,0,1}, DEG2RAD(30));
+    //mesh->MoveTo({0, 1, 0.5});
+    mesh->FitInsideUnitCube();
+    mesh->MoveTo({0,0,1});
+    scene.Add(mesh);
     scene.Add(new Sphere({0,0,-999}, 999, floor_material));;
-    scene.Add(new Sphere({3,1,4}, 1, new DiffuseLight({7.5, 6.70143, 6.03107})));
+    scene.Add(new Sphere({3,1,4}, 2, new DiffuseLight(ColorTemperature(5000)*7)));
     scene.Build();
 
     Renderer renderer(&scene, &cam, w, h, num_samples);
